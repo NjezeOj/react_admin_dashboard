1302b0d2d3672cd29e2f2be2472cef889f2977f4
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-06 23:27:48 +0100

    New Commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.css"
-import { LineStyle, Timeline,  TrendingUp } from "@material-ui/icons";
+import { LineStyle, Timeline, TrendingUp, Person, AttachMoney, BarChart, MailOutline, ChatBubbleOutline, WorkOutline, ErrorOutline } from "@material-ui/icons";
 
 const Sidebar = () => {
     return <div className="sidebar">
@@ -7,34 +7,72 @@ const Sidebar = () => {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <LineStyle/>
+                        <li className="sidebarListItem active">
+                            <LineStyle className="sidebarIcon"/>
                             Home
                         </li>
                         <li className="sidebarListItem">
-                            <Timeline/>
+                            <Timeline className="sidebarIcon"/>
                             Analytics
                         </li>
                         <li className="sidebarListItem">
-                            <TrendingUp/>
+                            <TrendingUp className="sidebarIcon"/>
                             Sales
                         </li>
                     </ul>
                 </div>
                 <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">Dashboard</h3>
+                    <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <LineStyle/>
-                            Home
+                        <li className="sidebarListItem active">
+                            <Person className="sidebarIcon"/>
+                            Users
                         </li>
                         <li className="sidebarListItem">
-                            <Timeline/>
+                            <Timeline className="sidebarIcon"/>
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon"/>
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChart className="sidebarIcon"/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Notifications</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <MailOutline className="sidebarIcon"/>
+                            Mail
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            Feedback
+                        </li>
+                        <li className="sidebarListItem">
+                            <ChatBubbleOutline className="sidebarIcon"/>
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Staff</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <WorkOutline className="sidebarIcon"/>
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
                             Analytics
                         </li>
                         <li className="sidebarListItem">
-                            <TrendingUp/>
-                            Sales
+                            <ErrorOutline className="sidebarIcon"/>
+                            Reports
                         </li>
                     </ul>
                 </div>
