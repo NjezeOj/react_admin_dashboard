0ceb6475368152547b9de69e2a0beb86c20d3f0c
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-08 15:08:24 +0100

    New Commit

--- src/dummyData.js
+++ src/dummyData.js
@@ -49,3 +49,18 @@ export const userData = [
     }
 
 ];
+
+
+export const userRows = [
+    { id: 1, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 2, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 3, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 4, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 5, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 6, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 7, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 8, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 9, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+    { id: 10, userName: 'Jon Snow', avatar: "https://www.w3schools.com/howto/img_avatar.png", email: "jon@gmail.com", status: "active", transaction: "$120.00" },
+
+];
